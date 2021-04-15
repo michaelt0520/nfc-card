49ad07ebab952b75c9f7b2fb15ddc37520dba36b
Nghia Tran <tran.nghia@1pac.vn>
2021-04-15 10:09:01 +0700

    fix vue warning

--- web/src/views/Personal.vue
+++ web/src/views/Personal.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="text-gray-900 leading-normal tracking-wider bg-cover" style="background-image: url('https://source.unsplash.com/1L71sPT5XKc')">
     <div class="max-w-4xl flex items-center h-screen flex-wrap mx-auto">
-      <div class="profile w-full lg:w-3/5 rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
-        <div class="p-4 md:p-12 text-center lg:text-left">
+      <div class="profile w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+        <div class="p-4 md:p-12 text-left">
           <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
-          <h1 class="text-3xl font-bold pt-8 lg:pt-0">{{ card.User.name }}</h1>
+          <h1 class="text-3xl font-bold pt-8 lg:pt-0">{{ card.User?.name }}</h1>
           <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
-          <p class="pt-4">{{ card.User.email }}</p>
-          <p class="pt-2">{{ card.User.Company.name }}</p>
-          <p class="pt-2">Nghia</p>
+          <p class="pt-4">Company name: {{ card.User?.Company.name }}</p>
+          <p class="pt-2">Company website: {{ card.User?.Company.website }}</p>
+          <p class="pt-2">Company address: {{ card.User?.Company.address }}</p>
         </div>
       </div>
 
       <div class="w-full lg:w-2/5">
-        <img :src="card.User.avatar" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
+        <img :src="card.User?.avatar" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
       </div>
     </div>
   </div>
@@ -25,20 +25,26 @@ import { mapState, mapActions } from 'vuex'
 export default {
   name: 'Personal',
 
+  data () {
+    return {
+      test: {}
+    }
+  },
+
   computed: {
-    ...mapState('cards', ['card']),
+    ...mapState('cards', ['card'])
   },
 
   methods: {
     ...mapActions('cards', ['getCard']),
   },
 
-  created() {
-    this.getCard(this.$route.params.code)
+  mounted() {
+    if (process.env.NODE_ENV === 'production') window.history.replaceState({}, '', `/personal/${this.card.User?.username}`)
   },
 
-  mounted() {
-    if (process.env.NODE_ENV == 'production') window.history.replaceState({}, '', `/personal/${this.card.User.username}`)
+  created() {
+    this.getCard(this.$route.params.code)
   }
 }
 </script>
